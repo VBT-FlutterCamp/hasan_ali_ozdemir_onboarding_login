2faf6609cecf0e089dbcedf919abf36a9f53a7f6
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-17 02:04:24 +0300

    constants

---
+++ lib/view/onboarding/onboarding_const.dart
@@ -0,0 +1,20 @@
+const int bottomFlex = 4;
+const int topFlex = 4;
+const List<String> onboardingAssetList = [
+  "assets/onboarding/empty_street.svg",
+  "assets/onboarding/social_distancing.svg",
+  "assets/onboarding/wash_hands.svg",
+];
+
+const List<String> onboardingTitleList = [
+  "Title1",
+  "Title2",
+  "Title3",
+];
+
+const List<String> onboardingDescList = [
+  "Title1Title1Title1Title1Title1Tititle1Tiitle1Title1Title1Tititle1Title1Title1Tititle1Title1Title1Tiitle1Tititle1Title1Title1Tititle1Title1Title1Title1Title1Title1Title1",
+  "TitlTitle1TitTitle1Title1le1Title1Title1Title1Title1Title1e2",
+  "TitlTitle1Title1Title1Title1Title1Title1Title1Title1Title1Title1e3",
+];
+String skip = "Skip";
