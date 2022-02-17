8a2c7a1cdf98168f2134f3f65c1730436d643705
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-17 14:57:00 +0300

    constant update

--- lib/view/auth/auth_const.dart
+++ lib/view/auth/auth_const.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 String welcome = "Welcome Back";
 String authTitle = "VBT Camp";
+String emailHint = "Email Adress";
+String passwordHint = "Password";
+String forgotPassword = "Forgot password?";
+
+String loginButton = "Login";
+String registerButton = "Register";
+
+String convertToRegister = "Don't have an account?";
+String convertToLogin = "Already have an account?";
+
+String convertToRegisterButton = "Create";
+String convertToLoginButton = "Login";
+
+Icon secureIcon = Icon(Icons.visibility_off);
+Icon nonsecureIcon = Icon(Icons.visibility);
